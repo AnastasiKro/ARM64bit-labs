d5cd106a28f092753ab5727859a7c8e15f204c69
yaAvtobus <62606871+yaAvtobus@users.noreply.github.com>
2021-04-28 19:22:44 +0300

    Add files via upload
    from file

--- base.s
+++ base.s
@@ -5,19 +5,24 @@ mes:
 	.equ len, .-mes
 eq:
 	.ascii "="
+base:
+	.ascii "base64"
 dict:
 	.string "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 filename:
 	.ascii "file"
+	.equ flen, 4
 	.equ	fname, 16
-	.equ	fd, 24
-	.equ	buf, 32
+	.equ	fd1, 24
+	.equ	fd2, 32
+	.equ	buf1, 40
+	.equ	buf2, 112
 	.text
 	.align 2
 	.global _start
 	.type	_start, %function
 _start:
-	adr	x0, mes
+	//adr	x0, mes
 	mov	x10, #0
 	adr	x15, dict
 	adr	x17, eq
@@ -26,49 +31,66 @@ _start:
 	mov	x16, #5028
 	sub 	sp, sp, x16
 	stp	x29, x30, [sp]
+	mov	x0, #-100
+	adr 	x1, filename
+	mov 	x2, #2
+	mov	x8, #56
+	svc	#0
 	mov	x29, sp
-	mov 	x9, buf
+	str	x0, [sp, fd1]
+	adr	x18, base
+	str	x18, [sp, #20]
+	add	x1, x29, buf1
+	mov	x2, #72
+	mov	x8, #63
+	svc	#0
+	mov	x29, sp
+	mov	x19, x0 
+	mov 	x9, buf2
 	mov 	x5, #0
-	mov	w14, #0
+	mov	x14, #0
 	b 	L0
 L0:
-	ldrb	w11, [x0, x10]
-	cmp	w14, len
+	add	x10, x14, buf1
+	ldrb	w11, [sp, x10]
+	cmp	x14, x19
 	bge	L3
 	lsr	x1, x11, #2
 	uxtw	x1, w1
-	add	x10, x10, #1
-	add	w14, w14, #1
+	//add	x10, x10, #1
+	add	x14, x14, #1
 	lsl	x2, x11, #4
 	lsl	x7, x1, #6
 	sub	x2, x2, x7
-	cmp	w14, len
+	cmp	x14, x19
 	bge	0f
-	ldrb	w12, [x0, x10]
+	add	x10, x14, buf1
+	ldrb	w12, [sp, x10]
 	lsr	x7, x12, #4
 	uxtw	x7, w7
 	add	x2, x2, x7 //got 2
 	lsl	x7, x7, #4
 	sub 	x3, x12, x7
 	lsl	x3, x3, #2
-	add	x10, x10, #1
-	add	w14, w14, #1
-	cmp 	w14, len
+	//add	x10, x10, #1
+	add	x14, x14, #1
+	cmp 	x14, x19
 	bge	1f
-	ldrb	w13, [x0, x10]
+	add	x10, x14, buf1
+	ldrb	w13, [sp, x10]
 	lsr	x8, x13, #6
 	uxtw	x8, w8
 	add	x3, x3, x8 //got 3
 	lsl	x8, x8, #6
 	sub	x4, x13, x8 //got 4
 	add	x5, x5, #4
-	add	w14, w14, #1
+	add	x14, x14, #1
 	b 	L2
 0:
-	add	w14, w14, #1
+	add	x14, x14, #1
 	mov	w3, #0
 1:
-	add	w14, w14, #1
+	add	x14, x14, #1
 	mov	w4, #0
 	add	x5, x5, #4
 	b 	L2
@@ -87,8 +109,8 @@ L2:
 	ldrsb	w6, [x15, x4]
 	strb	w6, [x29, x9]
 	add	x9, x9, #1
-	mov	x10, #0
-	add	x0, x0, #3
+	//mov	x10, #0
+	//add	x0, x0, #3
 	b L0
 0:
 	mov	x29, sp
@@ -102,21 +124,30 @@ L2:
 L3:	
 	mov	x0, #-100
 	adr	x1, filename
-	mov	x2, #2
+	mov	x2, #0xc0
 //	mov	x3, #2
 	mov	x8, #56 //open file
 	svc 	#0
-	str	x0, [sp, fd]
+	mov	x8, #57
+	svc 	#0
+	mov	x0, #-100
+	mov	x2, #2
+	mov 	x8, #56
+	svc	#0
+	str	x0, [sp, fd2]
 	ldp 	x29, x30,[sp]
-	ldr	x0, [sp, fd]
+	ldr	x0, [sp, fd2]
 	//mov 	x0, #2
-	add	x1, sp, #32
+	add	x1, sp, buf2
 	//adr	x1, mes
 	mov	x2, x5
 	//ldr	x1, [sp, buf]
 	mov 	x8, #64
 	svc	#0
-	ldr	x0, [sp, fd]
+	ldr	x0, [sp, fd1]
+	mov	x8, #57
+	svc 	#0
+	ldr	x0, [sp, fd2]
 	mov 	x8, #57
 	svc	#0
 L6:
